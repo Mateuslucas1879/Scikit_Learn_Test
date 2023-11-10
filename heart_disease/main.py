39f6874694539e1341c68ab5e2535fe592e7630e
Mateus Lucas <mateuslucas1879@gmail.com>
2023-11-10 13:45:35 -0300

    Atualização de Libs

--- heart_disease/main.py
+++ heart_disease/main.py
@@ -4,5 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
+df = pd.read_csv("heart-disease.csv")
+print(df.head())
 
---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlob.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("heart-disease.csv")
+df.head()
