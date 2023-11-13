c227bc1bb5dd141813412d65c33d4eb495fb722b
Mateus Lucas <mateuslucas1879@gmail.com>
2023-11-13 12:52:24 -0300

    Atualização

--- heart_disease/__init__.py
+++ heart_disease/__init__.py
@@ -1,8 +1,22 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.metrics import classification_report
 
+df = pd.read_csv("heart-disease.csv")
 
+print(df.head())
 
+X = df.drop("target", axis=1)
+y = df["target"]
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+#print(X_train.head())
+#print(X_test.head())
+
+model = RandomForestClassifier(random_state=1)
+
+model.fit(X_train, y_train)
+Y_pred = model.predict(X_test)
+#print(classification_report(y_test, Y_pred))
--- heart_disease/main.py
+++ heart_disease/main.py
@@ -2,8 +2,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+#from sklearn.model_selection import train_test_split
 
-
+# MOSTRA A TABELA DE FORMA COMPLETA
 df = pd.read_csv("heart-disease.csv")
 print(df.head())
+
+
+# SEGUNDA PARTE DO CODIGO 
+print("=-"*70)
+target_counts = df["target"].value_counts()
+print(target_counts)
+print("=-"*70)
+
+# TERCEIRA PARTE DO CODIGO
+target_counts03=df["target"].value_counts(normalize=True)
+print(target_counts03)
+
+#QUARTA PARTE DO CODIGO
+print("=-"*70)
+target_counts04 = df["target"].value_counts()
+print(target_counts04)
+
+# QUINTA PARTE DO CODIGO
+print("=-"*70) 
+target_counts05 = df.info()
+print(target_counts05)
+print("=-"*70)
+
+#SEXTA PARTE DO CODIGO
+
+
 
